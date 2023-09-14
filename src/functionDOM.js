8ecf979a628aa0e26f8dbafb54a1c20239668c1a
Vexli <74198817+Vexli@users.noreply.github.com>
2023-09-14 16:34:21 +0200

    update Menu and add images
    - added menu images
    - converted background CSS to addImgBackground()

--- src/functionDOM.js
+++ src/functionDOM.js
@@ -1,6 +1,6 @@
 /* FUNCTION Defintion */
 /* Add Specified Element */
-export function addElement(parent,type,arrClass,css,text,id) { // Create Element in DOM dynamically
+export function addElement(parent,type,arrClass,css,text,arrAttribute,arrValue) { // Create Element in DOM dynamically
   let eleParent = document.getElementById(parent);
   let ele = document.createElement(type);
   for (let i = 0; i < arrClass.length; i++) {
@@ -12,8 +12,8 @@ export function addElement(parent,type,arrClass,css,text,id) { // Create Element
   if (text.length > 0) {
     ele.textContent = text;
   }
-  if (id.length > 0) {
-    ele.setAttribute('id',id);
+  for (let i = 0; i < arrAttribute.length; i++) {
+    ele.setAttribute(arrAttribute,arrValue);
   }
 
   eleParent.appendChild(ele);
@@ -25,4 +25,10 @@ export function delAllElement(elementParent) {
   while (ele.hasChildNodes()) {
     ele.removeChild(ele.lastChild);
   }
+}
+
+/* Add Background Image */
+export function addImgBackground(element, source) {
+  let ele = document.getElementById(element);
+  ele.style.background = `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${source}) center/cover no-repeat`;
 }
--- src/loadMenuEn.js
+++ src/loadMenuEn.js
@@ -1,15 +1,41 @@
 /* IMPORT */
-import { addElement } from './functionDOM.js';
+import { addElement, addImgBackground } from './functionDOM.js';
+import imgBurger from './asset/img/burger.jpg';
+import imgDueruem from './asset/img/dueruem.jpg';
+import imgKebab from './asset/img/kebab.jpg';
+import imgWrap from './asset/img/wrap.jpg';
 
 /* FUNCTION Defintion */
 export function loadMenuEn(){
-  addElement("content","div",["align-items-center","col-12","container-fluid","d-flex","justify-content-center","p-0","vh-100"],"","","div-Container");
-  addElement("div-Container","div",["flex","justify-content-center","row","text-center","m-0"],"width:100%","","section");
-  addElement("section","h1",[],"","Gleis 1 Dürüm","Gleis1");
-  addElement("section","div",["flex","justify-content-center","d-flex","text-center","col-11","col-md-8","col-lg-6"],"width:100%","","tab");
-  addElement("tab","h3",[],"padding:2vw 2vw 0vw 2vw;","Home","home");
-  addElement("tab","h3",[],"padding:2vw 2vw 0vw 2vw;","Contact","contact");
-  addElement("section","br",[],"","","");
-  addElement("section","h3",[],"padding:2vw;","Menu","h3-menu");
-  addElement("section","div",["flex","justify-content-center","row","text-center","m-0"],"width:100%","","menu-options");
+  console.log('test');
+  addElement("content","div",["container-fluid","vh-100","d-flex","flex-column","align-items-center"],"overflow-y:auto;","",["id"],["container"]);
+  addElement("container","div",["row","text-center"],"","",["id"],["brand"]);
+  addElement("brand","h1",[],"padding-top:4vh;","Gleis 1 Dürüm",["id"],["Gleis1"]);
+  addElement("container","div",["text-center","d-flex","justify-content-center"],"","",["id"],["nav"]);
+  addElement("nav","h3",[],"padding:2vh;","Home",["id"],["home"]);
+  addElement("nav","h3",[],"padding:2vh;","Contact",["id"],["contact"]);
+  addElement("container","div",["row","text-center"],"","",["id"],["titel"]);
+
+  /* Menu Section */
+  addElement("titel","h2",[],"","Menu",["id"],["the-menu"]);
+  addElement("container","div",["d-flex","justify-content-center","flex-wrap"],"","",["id"],["con-menu"]);
+  addElement("con-menu","div",["text-center","align-items-center","menu-item"],"","",["id"],["dueruem"]); //Dürüm
+  addImgBackground("dueruem",imgDueruem);
+  addElement("dueruem","h4",[],"","Durum",[],[]); //Titel
+  addElement("dueruem","p",["p-menu"],"","Savor the goodness of our savory dürüm! Packed with tender marinated meat, crisp veggies, and aromatic spices, it's the perfect handheld delight.",[],[]); //Copy
+  addElement("con-menu","div",["text-center","align-items-center","menu-item"],"","",["id"],["wrap"]); //Wrap
+  addImgBackground("wrap",imgWrap);
+  addElement("wrap","h4",[],"","Wrap",[],[]); //Titel
+  addElement("wrap","p",["p-menu"],"","Wrap and roll into flavor with our scrumptious wrap! Grilled goodness in a soft flatbread - meat, veggies, and zesty sauces bundled together for a satisfying meal.",[],[]); //Copy
+  addElement("con-menu","div",["text-center","align-items-center","menu-item"],"","",["id"],["kebab"]); //Kebab
+  addImgBackground("kebab",imgKebab);
+  addElement("kebab","h4",[],"","Kebab",[],[]); //Titel
+  addElement("kebab","p",["p-menu"],"","Indulge in kebab bliss with juicy, grilled skewers, served with fluffy rice and tangy tzatziki. It's a sensational flavor experience.",[],[]); //Copy
+  addElement("con-menu","div",["text-center","align-items-center","menu-item"],"","",["id"],["burger"]); //Burger
+  addImgBackground("burger",imgBurger);
+  addElement("burger","h4",[],"","Burger",[],[]); //Titel
+  addElement("burger","p",["p-menu"],"","Get ready for a burger bonanza like no other! Sink your teeth into our mouthwatering burger, where juicy patties meet fresh veggies, melted cheese, and secret sauces, all hugged by a soft, toasted bun. It's a flavor explosion in every bite, and your taste buds are invited to the party!",[],[]); //Copy
+
+  /* Additional Info*/
+  addElement("container","p",[],"","All menu items are available with your choice of Beef (CH), Chicken (FR), or Falafel",[],[]);
 };
